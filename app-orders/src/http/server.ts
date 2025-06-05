95547ac5916ba442b657c2a478ac5cbfa4ba8529
João Gabriel Ulian <joaogabrielulian@gmail.com>
2025-06-04 22:15:58 -0300

    Configure orders database

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,11 +1,14 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { randomUUID } from "node:crypto";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
+import { db } from "../db/client.ts";
+import { schema } from "../db/schemas/index.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -29,10 +32,13 @@ app.post(
   },
   async (request, reply) => {
     const { amount } = request.body;
-    console.log("Creating an order with amount", amount);
-    return reply.status(200).send({
-      message: "Order created successfully",
+    const orderId = randomUUID();
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId: "B9176D35-7276-4255-A323-D825CAEE03B5",
+      amount,
     });
+    return reply.status(201).send();
   }
 );
 
